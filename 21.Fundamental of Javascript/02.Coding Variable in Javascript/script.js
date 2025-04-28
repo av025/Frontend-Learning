adbffc2557d05da73018c64bb4067b01c2d6ae05
Aayush Vyas <aayush25vyas@gmail.com>
2025-04-28 17:00:45 +0530

    Creating varaibles, naming rules and convention in javascript

---
+++ 21.Fundamental of Javascript/02.Coding Variable in Javascript/script.js
@@ -0,0 +1,41 @@
+//! Coding Variables in Javascript 
+//* As we know that variable was contanier or bucket to store our program data and with the help of variable we give name or identifier to that data for reference and we can change or rechange the value of variable also. 
+
+//? There was 3 ways to create variables in javascript : 
+
+//! 1st. With the help of "var" keyword 
+//* Syntax : var <variable name> = <value of variable>;
+//  Example of var keyword variable :- 
+var marks = 75; // variable One 
+var score = 300; // variable Two
+var playerName = "Aayush Vyas"; 
+
+//? Why we use Semicolon in programming language so in programming language we use semi-colon to end that line means when we use semi-colon than it become one single instruction to our programming language like this var playerName = "Aayush Vyas" was statement of instruction that to create variable.
+//* In javascript semi-colon are optional what to use semi-colon was in javascript was standard practice for creating document of our code and for readable puropse also. 
+
+//! 2nd. With the help of "let" keyword 
+//* Syntax : var <variable name> = <value of variable> 
+// Example of let keyword variable :- 
+let age = 26;  // variable three 
+// let anotherAge = 30;   variable Four 
+
+//! 3rd. With the help of "const" keyword 
+//* Syntax : const <variable name> = <value of variable> 
+// Example of const keyword variable :- 
+const PI_VALUE = 3.14;  // variable five 
+const totalPeople = 10; // variable six  
+
+
+//! Naming rules and convention of variable creation 
+
+//* Naming Rules 
+// 1st. Variables can contain small aplhabets, capital alphabets, underscore "_", dollar-sign symbal "$". 
+// 2nd. We don't have spaces between our variable and special characters or symbol was not allowed to create variable except underscore and dollar-sign special character. 
+// 3rd. We should always give meaningful names to our variable so it states that what was the purpose of particular variable and readers also read and understand the importance of variable and identity the meaning of the variable. 
+// 4th. Variable naming was not start with digits otherwise it give error we can use digits in between or at the last of variable.
+// 5th. We cannot use reserved words or predefined words like keywords for naming of variable, if we use it give error. 
+
+//* Conventions 
+// Mainly in programming the two important convention was used first was camelCase convention and second was snakecase convention 
+//! In javascript we mainly use camel-case convention 
+
