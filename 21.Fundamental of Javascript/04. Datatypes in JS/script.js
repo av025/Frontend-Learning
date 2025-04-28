90aad4125611eb6e8d3a3cd718cbbfe9852ac9b5
Aayush Vyas <aayush25vyas@gmail.com>
2025-04-29 00:47:29 +0530

    Introduction to Datatypes and types of datatype in JS

---
+++ 21.Fundamental of Javascript/04. Datatypes in JS/script.js
@@ -0,0 +1,47 @@
+//! Datatypes in Javascript ? 
+//* As we know that our programming language contain the data and instructions also so this data of many type which we categorise by divide those data into group which was called as datatypes.
+
+//? What all values we can store in javascript ? 
+
+//* 1st. Number type : We can have numbers type like 10 , -3 , 12.5, 3.66, 4.56, -999 etc. 
+// example 
+var myAge = 26;
+
+//* 2nd. String type : The group of characters or sequence of characters are called strings. We can write strings in javascript by three types first with the help of single quote 'Aayush Vyas', with the help of double quote "Aayush Vyas"and last with the help of literals or we say string literals in this we use back ticks `Aayush Vyas`   
+// example 
+var myName = "Aayush Vyas"; 
+
+//* 3rd. Boolean type : With the help of boolean type we can stored the logical data in boolean type we have only two value which are true and false that are keyword in programming language and this true and false boolean value are the smallest value among all. 
+// example 
+var anyCondition = true;
+
+//* 4th. Undefined type : this was special type  in which in starting or in any particular situation its type was not defined but later we can define data to this type.
+var status = undefined; 
+
+//* 5th. Objects type : With the help of objects we can store complex data or we say store data more than one in the form of key-value pair than we used objects
+// example 
+//* first object
+const userOne = {
+    name:"Aayush Vyas",
+    age:26, 
+    email:"aayush123@email.com",
+    planPurchase:true,
+    planType:"premieum" 
+}
+
+//* second object 
+const userSecond = {
+    name:"Kratik Vyas",
+    age:23,
+    email:"kratik@vyasemail.com",
+    planPurchase:false,
+    planType:"trail"
+} 
+//! This key was should be unique  
+
+//! Types of Datatype 
+//* There was mainly two types of datatype first was primitive and second was non-primitive datatype in programming language 
+
+//? Primitive datatypes are those datatype which was inbuit and atomic in nature means they are not depend on others for example numbers type they are independent. 
+
+//? Non-primitive datatype are those which derive from primitive datatype and it was composition of other datatypes for example objects in which we store other datatypes more than one in the form key-value pair.
