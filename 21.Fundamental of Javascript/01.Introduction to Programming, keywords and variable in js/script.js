33e6ca0dcef4332b7ac34a47c09477d6771ea7f1
Aayush Vyas <aayush25vyas@gmail.com>
2025-04-28 15:29:40 +0530

    Introduction to JS Programming, Keywords and Variables in programming language

---
+++ 21.Fundamental of Javascript/01.Introduction to Programming, keywords and variable in js/script.js
@@ -0,0 +1,24 @@
+//! Introduction to JS Programming , Keywords and Variables in programming language 
+
+//? What is Javascript ? 
+
+// Javascript was multi-paradigm programming language in which we can do procedural prgoramming , functional programming and also object oriented programming.  
+
+//* Understand with Analogy 
+// If we want to play the online game any online game like pacman or tic-tac-toe so we go to browser and inside browser we search tic-tac-toe game and going to that website and start playing that and if we refresh the game or refresh that page than our game start from the begining.
+//! So our point is that any peice of software we building or building games like tic-tac-toe so they have to store there data or information which are the score of players or in tic-tac-toe game in which grid the user mark and in which computer mark there cross-zero so to process our software or game we need some space for our memory to store the information and data for running the process of our application. 
+
+//* So we understand that if we want to create any software or game so firstly we write code of software in which we write the program where all the logic was written. If we want to run our software or run the game so this software and game was load inside the RAM which was our primary memory to help our system to run the process of running application and allocate required temporary memory to process our software, application or game also when we load our program in RAM than it was called "Process" 
+
+//! We can say that whenever we load our program in RAM "Random Access Memory" is called as "Process" 
+
+// For example if you want to open browser in our machine so we click on our browser icon to open any browser like google chrome in our machine behind the scence RAM "Random Access Memory" load that Google chrome software program all the peices of code which was needed to run google chrome load in our Primary memory Storage RAM which we download at the time of downloading browser from internet and than install it create some space for our browser data and information also.  
+
+//! This RAM Memory Storage was volatile means when we switch off our machine or device the data and information which was store in RAM because of allocation of memory for our program to store information and data was wipped out if we switch off similarly if we switch on again than this process starting from begining similar like the tic-tac-toe game when we refresh the browser window in start from begining again.
+
+//? What was Keywords in programming language ? 
+//* In programming language there was some reserved or pre-defined words are present which convey some meaning to perform some task so that words instruction was understand by programming language by itself and we can't change the meaning of it or can't do modification with those some words, This reserved or predefined words are called Keywords.
+
+//? How can we stored Data in our program ? 
+//* As we know our program was load in RAM "Random Acess Memory" whenever we want to run our program, software, or game in RAM Primary memory storage the some space was allocated or memory allocated to store our program data and instruction so this data was stored with the help of container or bucket that was called as Variable, With the help of variable we can give the name to our memory data and this data was stored in sequence in RAM means we give the identtifier to our memory location or memory address by giving variable name and we can use that as reference and change or rechange the value of that memory address by the reference of the variables.  
+
