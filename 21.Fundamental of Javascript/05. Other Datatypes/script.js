42c6923f0b58069c87e250c4ddd2d0e59d24f407
Aayush Vyas <aayush25vyas@gmail.com>
2025-04-29 17:32:25 +0530

    Other Datatypes in JS

---
+++ 21.Fundamental of Javascript/05. Other Datatypes/script.js
@@ -0,0 +1,12 @@
+//! Other Datatypes in JS 
+
+//* Null Type : When we say null which means nothing when we represent null type it actually means empty value on other hand we have other type which was undefined which we see previously which also used to give empty value for specific situation than after that we can give assign value to that . 
+
+// let see example of null type and undefined type 
+var myAge; 
+//? Above variable myAge was undefined because we don't assign value just declare variable now. Later we defined or assign the value also 
+//! Here we assign the value implicit manner we can use undefined keyword also to assign the value which was explicitly giving value.
+
+var disibility = null; //* Here we assign the value null to variable disbility 
+console.log(myAge); //! Output : undefined 
+console.log(disibility); //! Output : null 
