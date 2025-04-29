a0a96f2911d1e7e6c151c8f07ec3819968712680
Aayush Vyas <aayush25vyas@gmail.com>
2025-04-29 20:40:10 +0530

    Special Characters in Javascript

---
+++ 21.Fundamental of Javascript/06.Special Characters/script.js
@@ -0,0 +1,21 @@
+//! Special Characters in Javascript 
+//* In all programming language there was speical characters which help us in different ways let see this special characters : 
+
+// ? 1st. Escape Sequence "new line character"
+// With the help of this new line character we can do escape sequence which start our execution of code with new line. 
+//! Syntax of New-line character "\n" with the help of backslash "\" + n we can create new-line character or escape sequence 
+//* example of Escape sequence 
+console.log("The new product launch by apple was iphone 16 \n which was launch on this week") 
+
+//? 2nd. Tab space 
+// With the help of tab space character we can give tab space between our string or in starting 
+//! Syntax for giving tab space was "\t" with the help of backslash "\" + t we can create tab space in our strings 
+//* example of Tab space 
+console.log("My name is \t Aayush Vyas")  
+
+//* Another example of escape sequence : 
+let age = 25; 
+let name = "Aayush Vyas";
+
+console.log(name, "\n", age)
+//! We use newline character inside the string 
