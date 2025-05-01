07ea55cfad6a2be9a0d9bc7768ae20c94b3a62f8
Aayush Vyas <aayush25vyas@gmail.com>
2025-05-01 17:16:54 +0530

    Second problem on conditionals

---
+++ 21.Fundamental of Javascript/19. Problems on conditionals/second_problem.js
@@ -0,0 +1,15 @@
+//! Problem Solving - Another problem to solve on if the number x in even or odd 
+
+//* input x = 27  output : Odd 
+//* input x = 54  output : Even 
+
+let x = 27;  
+
+// reassign the value 54 
+x = 54;
+
+if(x % 2 === 0 ) { 
+ console.log("Given number x is even")
+} else {
+    console.log("Given number x was odd")
+}
