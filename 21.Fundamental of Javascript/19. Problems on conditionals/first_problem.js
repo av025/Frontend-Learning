2f2613b9f97e7f2579ce5709c720164d6f081f70
Aayush Vyas <aayush25vyas@gmail.com>
2025-05-01 17:04:43 +0530

    First problem of conditional

---
+++ 21.Fundamental of Javascript/19. Problems on conditionals/first_problem.js
@@ -0,0 +1,12 @@
+//! Problem Solving  - Given a Number x , Check if the number was even or odd 
+
+let x = 27; 
+
+//* reassign the value 
+x = 36;
+
+if(x % 2 !== 0) {
+    console.log("Given x number was odd."); //* This statement will execute when the value of x is 27 
+}else {
+    console.log("Given x number is even.") //* This statement will execute when the value of x is 36
+}
