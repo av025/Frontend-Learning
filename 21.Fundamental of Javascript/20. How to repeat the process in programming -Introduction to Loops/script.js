7f9b8183924328a52b89420d48192cd608cfafef
Aayush Vyas <aayush25vyas@gmail.com>
2025-05-03 14:32:53 +0530

    Introduction to Loops

---
+++ 21.Fundamental of Javascript/20. How to repeat the process in programming -Introduction to Loops/script.js
@@ -0,0 +1,22 @@
+//! How to repeat the process in programming - Introduction to Loops 
+
+//? Whenever we have to do things repeatedly in programming language we use loops with the help of loops we can repeat process for certain time or upto that task. 
+
+//* There was different type of loops : 
+
+//? Let see example of looping 
+// Write the program to prints the number from 1 to 10. 
+
+//! For Looping we use the first loop which was for-loop for looping 
+//* syntax of for-loop 
+//? for(looping variable , looping condition , incrementing / decrement variable) {
+//? 
+//?        Scope of for-loop  where we write our task to repeat the process 
+//?
+//?  } 
+
+//* Example of for loop 
+
+for(let number = 1; number <= 10 * 10; number++) {
+    console.log(number)
+}
