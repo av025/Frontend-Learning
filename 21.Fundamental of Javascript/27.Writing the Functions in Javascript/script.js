b4b8a27a6236573438fa08c41972c371ff3746bb
Aayush Vyas <aayush25vyas@gmail.com>
2025-05-07 23:43:18 +0530

    Writing functions in js

---
+++ 21.Fundamental of Javascript/27.Writing the Functions in Javascript/script.js
@@ -0,0 +1,40 @@
+//! Writing Functions in Javascript 
+
+//* First See Syntax of Function how we create function in JS 
+//! 1.We use the function keyword "function" 
+//! 2. Second we give identifer means name to our function than
+//! 3. Use circle parenthesis "()" 
+//! 4. Create scope of our function with curly braces "{}" 
+
+//? function function_identifier() {
+//?  // Block of Function Scope 
+//?  Here we write our code of instruction
+//!  Function returns something if we want to otherwise it return undefined 
+//? }
+//* Above we call the function calling......  
+
+
+//! Second part in function was calling function 
+//? function_identifier( )
+//* Second we call the function to execute..... 
+
+
+//? Let see with example how can we create Functions in Javascript 
+//* Create the function to check if the number even and odd 
+
+function isEvenOrOdd() {
+    let number = 20; 
+    
+    if(number % 2 === 0) {
+        return true;  //* In conditional we return this if even number 
+    }else {
+        return false;  //* In conditional we return this if odd number 
+    }
+} 
+
+
+if(isEvenOrOdd()) {
+    console.log("Number is Even !!!");
+} 
+
+
