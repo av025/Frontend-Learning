5c881389b10aca9cd5f3e61a2c6546c68e72046a
Aayush Vyas <aayush25vyas@gmail.com>
2025-05-07 16:42:44 +0530

    Reverse number printing with the help of while loop

---
+++ 21.Fundamental of Javascript/23.Print the reverse number with the help of while loop/script.js
@@ -0,0 +1,15 @@
+//! Print the numbers in reverse with the help of 20 
+
+//* Printing number from 20 to 1 
+
+let number = 20; 
+
+while(number > 0) { 
+
+    console.log(number) 
+    //* Print number 
+
+    //? looping variable condition
+    number -= 1;  
+
+}
