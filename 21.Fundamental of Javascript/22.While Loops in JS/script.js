e3004f73e700b26796fd01d1eb24558a21987e77
Aayush Vyas <aayush25vyas@gmail.com>
2025-05-07 16:30:20 +0530

    Introduction to While loops

---
+++ 21.Fundamental of Javascript/22.While Loops in JS/script.js
@@ -0,0 +1,32 @@
+//! While Loops in Javascript 
+// As we know that we understand that for repeating any task in programming than we used the method of looping in which there are different type of loops are present in programming to do looping. 
+
+//? Lets see our first loop which was "While loop" 
+//* Syntax of While loop 
+//! while(condition of while loop) {
+//!   "Block of While loop" 
+//!   
+//!     instruction of code  
+//!      
+//!   At last "increment of looping varialbe" 
+//!
+//!      } 
+
+
+//? Example of While loop 
+//* Looping variable 
+let number = 100; 
+
+console.log("Start of While loop");
+//* While loop 
+while(number > 0) {
+    //! While loop checking this condition upto it becomes false or upto it remain true
+
+    //* Printing variable i  
+    console.log(number);
+
+    //* decrement variable i 
+    number -= 1; 
+    
+} 
+console.log("End of Program");
