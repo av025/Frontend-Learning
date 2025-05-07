a48e9c4f561845d0c71ff484ffd8b1dcc4b0d48d
Aayush Vyas <aayush25vyas@gmail.com>
2025-05-07 20:51:13 +0530

    Introduction to Functions in JS

---
+++ 21.Fundamental of Javascript/26.Introduction to Functions/script.js
@@ -0,0 +1,19 @@
+//! Introduction to Functions 
+//* Function was the resuable block of code which takes some input as instruction of a specific task then process it and return output of that input.  
+//? Let see example of function let we have to calculate the square of any number 
+
+//! This was called function declaration
+function squareOfNumber() {
+    let number = 10; 
+    let squareOfNumber = number * number;
+    console.log(squareOfNumber);
+}
+
+//! This was called function calling 
+squareOfNumber();
+//* function only execute when we are calling the function only !!!!  
+
+
+//! In Programming language there are two types of function present 
+//* 1st. Inbuild functions which was provide by programming language 
+//* 2nd. Custom functions which was create by ourself 
