6f869ab9f4bb6f799ca25bb65fd6ee866ce79f5e
Aayush Vyas <aayush25vyas@gmail.com>
2025-05-08 02:01:05 +0530

    Exploring more things with functions

---
+++ 21.Fundamental of Javascript/31. Exploring things/script.js
@@ -0,0 +1,15 @@
+//! Exploring things 
+
+//* let see more example on function with arguments and parameters 
+
+//? Function declaration 
+function addNumbers(valueOne, valueTwo) {
+    const addingNumbers = valueOne + valueTwo; 
+    console.log(addingNumbers); 
+}
+
+//? Function Calling 
+addNumbers(20, 10 , 30); 
+//! Above what happen we pass another arguments when only two parameter was passed  
+//* Javascript silently ignore the third argument and process the starting of our two parameter arguments and give result of it....
+
