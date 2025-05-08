9664fbcace2417a422380342da74b7c96aa771d8
Aayush Vyas <aayush25vyas@gmail.com>
2025-05-09 01:48:37 +0530

    Entry Controlled and Exist Controlled

---
+++ 21.Fundamental of Javascript/37. Entry controlled and Exist Controlled/script.js
@@ -0,0 +1,30 @@
+//! Entry Controlled and Exist Controlled Loops in Programming Language 
+
+//? Entry Controlled Loops 
+//* The Entry controlled loops are those loops in which entry was control for example while and for loop 
+
+// While loop  
+let variableOne = 1; 
+
+while(variableOne <= 5) {
+    console.log(variableOne); 
+    variableOne++; 
+} 
+
+// For loop 
+for(let variableTwo = 0; variableTwo <= 5; variableTwo++) {
+    console.log(variableTwo);
+}
+
+
+//? Exist Controlled Loops 
+//* Exist controlled loops are those loops in which exist was control for example do-while loop 
+
+// Do-While loop 
+let variableThree = 1; 
+
+do {
+    console.log(variableThree); 
+    variableThree++; 
+}while(variableThree <= 5); 
+
