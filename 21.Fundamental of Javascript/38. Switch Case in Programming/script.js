ec13a7365c4bca9e2b4e6f4920e737fc749f405b
Aayush Vyas <aayush25vyas@gmail.com>
2025-05-09 15:06:46 +0530

    Break keyword

---
+++ 21.Fundamental of Javascript/38. Switch Case in Programming/script.js
@@ -0,0 +1,25 @@
+//! Switch Case Statement in Programming language 
+
+//* Switch case was also conditional statement also but here we give the expression or value to switch case parenthesis than we matches case in the switch block of code in simple term this switch case was also the conditional statement in which we do case matching with our expression or value which we can pass to switch case parenthesis. 
+
+
+//? Let see syntax of Switch Case 
+//! Switch(condition of switch case)  {
+//? Block of Switch Case  
+//!  
+//!   case one : 
+//!       logic or statement of case one 
+//!       break statement 
+//* This Break keyword was used to stop the execution of our switch case and come out from this block of code if we get desired  output or matching output. 
+//!  
+//!    case two : 
+//!      logic or statement of case two 
+//!      break statement 
+//!
+//!  case three:
+//!    logic or statement of case three 
+//!    break statement 
+//!
+//!   default:   
+//!     logic or default statement 
+//! }
