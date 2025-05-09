f7dc0aeac61378b7b3bb645fa7b8461e7e583c31
Aayush Vyas <aayush25vyas@gmail.com>
2025-05-09 21:08:43 +0530

    Introduction to Arrays

--- 21.Fundamental of Javascript/41.Introduction To Arrays/script.js
+++ 21.Fundamental of Javascript/41.Introduction To Arrays/script.js
@@ -9,3 +9,12 @@
 //* We create array  with the help of square brackets "[ ]" 
 //*  [Element-One , Element-Two, Element-Three, Element-Four]  
 
+
+//! Application of an array 
+//* Biggest example of array was Pictures we click  or taken by other devices so this internally this picture was the array of channels and those channels are Red color channel , blue color channel and greed color channel 
+
+//? This red color channel, blue color channel and green color channel in which we use small small pixels  of array which create our pictures  
+
+// So we say that pictures are the  array collections of RGB colors channel. 
+
+
