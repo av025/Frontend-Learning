631b3fc2939c981278ed058543ff8fe646b4cd6d
Aayush Vyas <aayush25vyas@gmail.com>
2025-05-09 18:03:07 +0530

    Introduction to Arrays

---
+++ 21.Fundamental of Javascript/41.Introduction To Arrays/script.js
@@ -0,0 +1,11 @@
+//! Introduction to Arrays in programming language 
+
+//? What is an Array in programming ?  
+// Let understand we have to store 10 numbers in our program so we can create the individual variable create to store number but that was also not good practice if similarly we have to store 100 numbers or more than that ?  
+
+//! We use Arrays was data structire  which store the larger data or collection of data in linear contiugous orientation in a memory  
+
+//? Let see syntax of array 
+//* We create array  with the help of square brackets "[ ]" 
+//*  [Element-One , Element-Two, Element-Three, Element-Four]  
+
