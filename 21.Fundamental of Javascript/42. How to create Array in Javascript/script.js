2ec7bb18f867ce15257681bf64e9b590e567ec8d
Aayush Vyas <aayush25vyas@gmail.com>
2025-05-09 23:13:03 +0530

    How to create Array in Javascript

---
+++ 21.Fundamental of Javascript/42. How to create Array in Javascript/script.js
@@ -0,0 +1,18 @@
+//! How to create Array in Javascript
+
+//* Let see with example how we can implement array in javascript we know that we create array with square brackets
+
+const myFirstArray = []; //? This was empty array because there was not single element in it.
+console.log(myFirstArray); //* Output was []
+
+const myNumbersArray = [1, 2, 3, 4, 5, 6]; //* Here we intialize the number array 
+console.log(myNumbersArray);   //* Output was [ 1,2,3,4,5,6]  
+//! The length of this myNumbersArray was 6 because it has 6 elements inside in it. 
+
+const myStringArray = ["ab", "cd", "ef", "gh","ij", "kl"];  //* Here we intialize the string array 
+console.log(myStringArray); //* Output was was ["ab","cd","ef","gh","ij","kl"] the length of this array was 6  
+
+const dynamicArray = [true, false, 1, 2 , 3, 4 ,"Aayush", "Vyas"];
+//! In Javascript as we know that losely type so array was dynamic in nature so we can store different datatype in it . 
+console.log(dynamicArray);  //* Output : [true, false, 1, 2, 3, 4,"Aayush", "Vyas"]  
+
