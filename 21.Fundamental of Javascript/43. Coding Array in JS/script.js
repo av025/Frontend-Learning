9fd960ae8da0eeb90d9082c25a102858630b9b9e
Aayush Vyas <aayush25vyas@gmail.com>
2025-05-11 01:36:45 +0530

    Coding example of array

---
+++ 21.Fundamental of Javascript/43. Coding Array in JS/script.js
@@ -0,0 +1,15 @@
+//! Coding Array in Javascript 
+
+//* Lets create array in javascript for example 
+let arrayOne = []; 
+//? Creating empty array 
+let arraySecond = ["ab","cd", "ef", "gh","ij", true, false, undefined]; 
+let arrayThird = ["aa", "bb", "cc", "dd"]; 
+let falsyValueArray = [false, null, "", NaN, undefined,0 , -0];
+
+// Let print value of all array 
+console.log( "arrayOne",arrayOne); //? Output : arrayOne []
+console.log("arraySecond",arraySecond); //? Output : arraySecond ["ab","cd","ef","gh","ij",true,false,undefined]
+console.log("arrayThird",arrayThird); //? Output : arrayThird ["aa", "bb", "cc", "dd"]
+console.log("falsyValueArray",falsyValueArray);  //? Output : falsyValueArray : [false, null, "", NaN, undefined, 0, -0]
+
