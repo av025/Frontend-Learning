dd97808c1ae45fc3b374e967e10700266343cb1f
Aayush Vyas <aayush25vyas@gmail.com>
2025-05-14 15:39:13 +0530

    Defination of Coercion

---
+++ 22. First Pillar of Javascript - Coercion/02. Defination of Coercion/script.js
@@ -0,0 +1,15 @@
+//! Defination of Coercion 
+//* What is Coercion in javascript 
+// Whenever we perform any operation , based on the input basically we convert or we say do type-conversion means convert one type to another type to input for operation.  
+
+//? This Type - Conversion was done by two types : 
+
+//* Explicit Type Casting 
+//* When we have to convert one type to another type manually by ourself than it was called Explicit Type-casting or Explicit Type-conversion for that we have some method inbuild to convert explicitly. 
+
+//* Implicit or Abstract Type Casting 
+//* The language internally based rules which automatically converts one type to another type is known as Implicit or Abstract Type casting. 
+
+//! This Implict or Abstract Type Casting was also know as Coercion 
+
+
