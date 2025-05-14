f2f40e6a7a4e7dd58e8eef00e65f95d4b21688f5
Aayush Vyas <aayush25vyas@gmail.com>
2025-05-14 15:21:34 +0530

    Abstract operation in javascript

---
+++ 22. First Pillar of Javascript - Coercion/01.Abstract operation/script.js
@@ -0,0 +1,6 @@
+//! Abstract Operation 
+//* 1. These are some of set of algorithms, that is present in the ecmascript does, but they are not available for usage in ecmascript or have abstraction for usage in ecmascript i.e we developers can't use those operation directly.
+
+//* 2. They are mentioned in the does to aid(help) the documentation only, in the ecmascript lot of things that are done by itself language only.   To Explain and understand what all the operation language do internally this abstract operations was added to ECMA Docs. 
+
+//! With the help of this Abstract operation we can understand what internally operation javascript done , why is done, how it was doen internal operation in javascript .... 
