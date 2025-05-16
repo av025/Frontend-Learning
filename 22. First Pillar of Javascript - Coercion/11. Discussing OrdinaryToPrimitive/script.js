72345331d04d3441a575ba275a7e9ca584f0fbbe
Aayush Vyas <aayush25vyas@gmail.com>
2025-05-16 23:01:57 +0530

    Discussing OrdinaryToPrimitive

---
+++ 22. First Pillar of Javascript - Coercion/11. Discussing OrdinaryToPrimitive/script.js
@@ -0,0 +1,7 @@
+//! Discussing OrdinaryToPrimitive 
+//* After all the algorithms that call and the steps we see in the ToPrimitive at last all the algorithms and step completed than after that this OrdinaryToPrimitive function was return which has our ToPrimitive "input" and "hint" of variable which we set in ToPrimitive function 
+
+//? It means that ToPrimitive returns this another operation which was OrdinaryToPrimitive let see syntax of this OrdinaryToPrimitive 
+
+//! OrdinaryToPrimitive(O,hint)
+// Here the OrdinaryToPrimitive O was the argument Object and hint was variable which was we set as preffered type from ToPrimitive abstract operation. 
