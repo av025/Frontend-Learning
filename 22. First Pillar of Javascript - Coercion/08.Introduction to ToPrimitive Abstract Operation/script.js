bccccb40285fc04bff63147be741c6ba1f67eaf4
Aayush Vyas <aayush25vyas@gmail.com>
2025-05-16 15:09:17 +0530

    Introduction to ToPrimitive Abstract Operation

---
+++ 22. First Pillar of Javascript - Coercion/08.Introduction to ToPrimitive Abstract Operation/script.js
@@ -0,0 +1,7 @@
+//! Introduction to ToPrimtive Abstract Operation 
+//* This ToPrimitve Abstract Operation takes two argument first was input type argument and second was preferred type optional argument.
+
+//? ToPrimitive(input[, preferredType]); 
+//! This ToPrimitive help to convert that input type into non-object type like primitive datatype like numbers , strings , boolean etc. 
+
+//* If we give input which was converted into multiple type input like type-one , type-two , type-three etc.  than here the prefered type was used among all which was optional argument
