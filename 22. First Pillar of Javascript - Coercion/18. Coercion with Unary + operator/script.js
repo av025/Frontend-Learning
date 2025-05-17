257ed783ade1d4e7d1d018a4d1a5c47da34b026f
Aayush Vyas <aayush25vyas@gmail.com>
2025-05-17 22:16:10 +0530

    Coercion with Unary + operator

---
+++ 22. First Pillar of Javascript - Coercion/18. Coercion with Unary + operator/script.js
@@ -0,0 +1,6 @@
+//! Coercion with Unary + operator
+
+// In this Unary addition operator it try to convert our other type into only number type let see example of it 
+console.log(+"5" , typeof +"5"); //* Output : 5 , number 
+console.log(+true, typeof +true); //* Output : 1, number 
+console.log(+"abcd", typeof +"abcd"); //* Output : NaN, number
