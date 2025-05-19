34c2856ff99d5d7288bae42c83537acb0dbf5ccc
Aayush Vyas <aayush25vyas@gmail.com>
2025-05-19 14:48:01 +0530

    Coercion Special Cases

---
+++ 22. First Pillar of Javascript - Coercion/26. Coercion special cases/script.js
@@ -0,0 +1,16 @@
+//! Coercion Special Cases : 
+
+//? As we see that for ToString abstraction we add empty string with our operand so it becomes string than concatenate with each other. 
+console.log("" + 0, typeof ("" + 0));  //* Output : 0 , type : string 
+console.log("" + (-0)); //* Output : 0 so, here we see that -0 was also convert into 0 string 
+console.log("" + []); //* Output : "" - Empty String 
+//? So our array was convert into empty string because it was empty array so it removes the square brackets 
+console.log("" + [1,2,3,4,5,6]); //* Output : 1,2,3,4,5,6  this + operator do coercion and convert array elements in string and also seperated with commas our array element. 
+//? What happen when we convert our falsy array element 
+console.log("" + [null, undefined]);  //* Output : , 
+//* When we perform ToString abstract operation it removes falsy value from our array 
+console.log("" + [1,2,null,3,undefined]); //* Output : 1,2,,3, 
+
+//? Some special cases with ToNumber abstract operation 
+console.log(0 - "010") //* Output was -10 because this string was convert into 010 which was 10 only and than we do 0 - 10 = -10 
+console.log( 0 - "O10"); //* Output was NaN because bymistake or to look how zero 0 and character 0 was handle in this concat implicit operation because both look like same. 
