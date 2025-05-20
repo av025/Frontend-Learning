f1d4d7c57818c6af26a875e83d98f0815a86c5b9
Aayush Vyas <aayush25vyas@gmail.com>
2025-05-20 14:55:10 +0530

    Compiled and Interpreted language in programming

---
+++ 23. Second Pillar of Javascript - Scopes/02.Javascript was interpreted or compiled language/script.js
@@ -0,0 +1,19 @@
+//! Javascript was Interpreted or Compiled language  
+//* Before understanding the nature of javascript first we understand what was means interpreted or compiled language 
+
+//?  Interpreted language 
+//* In an interpreted language, a tool or software (called an interpreter) processes the code line by line, converting it into machine language and then executing it immediately.
+
+//* This approach is generally slower than compiled languages, because instead of converting the entire code into machine language at once, the interpreter checks and executes each line one at a time, also checking for any syntax or runtime errors during the process. 
+
+//* Example : c , c++ etc 
+
+
+//? Compiled language 
+//* In a compiled language, a tool or software (called a compiler) processes the entire code in one go.
+//* It translates the whole code into machine language and creates an executable file.
+//* After that, the program is executed from this file.
+//* One drawback is that errors are only detected during the compilation phase, not line by line like in interpreted languages.
+//* However, compiled languages are usually faster than interpreted ones because the entire code is already converted into machine code before execution.
+
+//* Example : python , shell etc 
