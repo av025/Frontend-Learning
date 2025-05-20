f3dbbb7faa065bf55cd22336df3315239911bf52
Aayush Vyas <aayush25vyas@gmail.com>
2025-05-20 14:21:52 +0530

    What is Scopes in javascript

---
+++ 23. Second Pillar of Javascript - Scopes/01. What is Scopes/script.js
@@ -0,0 +1,8 @@
+//! What is Scopes
+//* In Javascript the scopes was most crucial and important concept which define the visibility and readability of local variables and function which we define in our code.  
+//* This scopes help to look and help us to find where our variables and function define , where we can access those function and variable and to also help to understand where we can't access those variable and function.
+
+//? Every thing "function variables" inside our code, will be used in one of the following 2 ways :- 
+// 1st. It was used to assign some value in it . 
+// 2nd. With the help of this variable and function we can reterived value from it. 
+
