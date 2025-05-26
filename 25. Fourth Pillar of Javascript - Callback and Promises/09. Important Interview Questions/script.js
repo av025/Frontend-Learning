2c5fa04dc5f833c5ea08df334601ac3e00029679
Aayush Vyas <aayush25vyas@gmail.com>
2025-05-27 00:27:50 +0530

    Interview Third Question

--- 25. Fourth Pillar of Javascript - Callback and Promises/09. Important Interview Questions/script.js
+++ 25. Fourth Pillar of Javascript - Callback and Promises/09. Important Interview Questions/script.js
@@ -16,14 +16,14 @@
 
 
 //? Interview Question Second : 
-console.log("Hello World !!!!!");
-setTimeout(function() {
-    console.log("Timer Done !!!!!"); 
-},0); 
-for(let i = 1; i <= 10000000000; i++) {
-    //* Looping Process 
-}; 
-console.log("End");
+// console.log("Hello World !!!!!");
+// setTimeout(function() {
+//     console.log("Timer Done !!!!!"); 
+// },0); 
+// for(let i = 1; i <= 10000000000; i++) {
+//      Looping Process 
+// }; 
+// console.log("End");
 
 //? Output : 
 //* "Hello World !!!!"
@@ -31,5 +31,29 @@ console.log("End");
 //*  "End"
 //*  "Timer Done"  
 //! Asynchronous func was always executed at last............. 
+
+//? Third Interview Question 
+console.log("Hellooo !!!!!"); 
+for(let i = 1; i <= 3; i++) {
+    setTimeout(function() {
+        console.log("Timer Done");
+    },10); 
+}; 
+for(let i = 1; i < 100000000; i++) {
+    // Looping process 
+}; 
+console.log("Ends"); 
+
+//? Output : 
+//* "Hello !!!!"; 
+//* First for-loop processing  and setTimeout function task was also complete 
+//* for-loop processing was complete 
+//* "Ends"
+//* Timer Done 
+//* Timer Done 
+//* Timer Done 
+//! Asynchronous function was always executed at last..........
+
+
 
 
