d545e5760dea97b505546e37b10ed2605f97ad14
Aayush Vyas <aayush25vyas@gmail.com>
2025-05-27 00:10:13 +0530

    Interview Question First

---
+++ 25. Fourth Pillar of Javascript - Callback and Promises/09. Important Interview Questions/script.js
@@ -0,0 +1,13 @@
+//! Important Interview Questions 
+
+//? Interview Question First : 
+console.log("Hello World !!!!");
+setTimeout(function() {
+   console.log("Timer Done !!!!"); 
+}, 0); 
+console.log("End");
+
+//? Output  : 
+//*  "Hello World !!!!" 
+//* "End"
+//* "Timer Done"
