f7bf31fc19bbb9554dca67cf627a9b5fc4b7bcae
Aayush Vyas <aayush25vyas@gmail.com>
2025-05-27 00:18:36 +0530

    Interview Question Second

--- 25. Fourth Pillar of Javascript - Callback and Promises/09. Important Interview Questions/script.js
+++ 25. Fourth Pillar of Javascript - Callback and Promises/09. Important Interview Questions/script.js
@@ -1,13 +1,35 @@
 //! Important Interview Questions 
 
 //? Interview Question First : 
-console.log("Hello World !!!!");
-setTimeout(function() {
-   console.log("Timer Done !!!!"); 
-}, 0); 
-console.log("End");
+
+// console.log("Hello World !!!!");
+// setTimeout(function() {
+//    console.log("Timer Done !!!!"); 
+// }, 0); 
+// console.log("End");
 
 //? Output  : 
 //*  "Hello World !!!!" 
 //* "End"
-//* "Timer Done"
+//* "Timer Done" 
+//! Asynchronous func was always executed at last..................
+
+
+//? Interview Question Second : 
+console.log("Hello World !!!!!");
+setTimeout(function() {
+    console.log("Timer Done !!!!!"); 
+},0); 
+for(let i = 1; i <= 10000000000; i++) {
+    //* Looping Process 
+}; 
+console.log("End");
+
+//? Output : 
+//* "Hello World !!!!"
+//*  Here Processing was done not anything was printed 
+//*  "End"
+//*  "Timer Done"  
+//! Asynchronous func was always executed at last............. 
+
+
