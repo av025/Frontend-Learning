281f83e774a1ad4fb2c70236a405969985b0daf1
Aayush Vyas <aayush25vyas@gmail.com>
2025-05-28 00:03:59 +0530

    Syntax of new promise

---
+++ 25. Fourth Pillar of Javascript - Callback and Promises/15. Syntax of new promise/script.js
@@ -0,0 +1,11 @@
+//! Syntax of new promise
+//* Let see how we create the syntax of new promise :
+
+//? We create the promises with the help of new keyword and we use Promise  Constructor function with capital P
+
+//* Syntax of Promise :
+new Promise(function (resolve, reject) {
+  //* Inside this Callback fn we write the time consuming task
+});
+//! This Promise constructor takes the argument as callback function.....
+//? In this callback function we are passing the the two parameter one was "resolve" and second was "reject", we can pass anything like a , b whatever but the meaning of this two parameter.
