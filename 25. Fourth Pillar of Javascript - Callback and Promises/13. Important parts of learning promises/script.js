7e46306fa24e25a28003a33691ea3b0239151a65
Aayush Vyas <aayush25vyas@gmail.com>
2025-05-27 20:03:23 +0530

    Important parts of learning promises

---
+++ 25. Fourth Pillar of Javascript - Callback and Promises/13. Important parts of learning promises/script.js
@@ -0,0 +1,10 @@
+//! Important parts of learning promises : 
+
+//* We have to learn two important thing to learn and understand the promise : 
+//? 1st. How we will create the promise ? 
+//? 2nd. How to consume the promise ?  
+
+//* To understand above points we have to understand..... what was the promises ? 
+//? Let understand promises in general term assume that if anyone prmoise you something like they give you some treat when you got A+ grade in your exam if you fulfill the promise that person give you treat and there was anthor condition if you don't get grade A+ grade than you can't get treat so youll don't fullfil the promise 
+
+//* Similarly in techincal term promises work in similar model when we fetch data from backend or third-party api than we use promise this promise will fullfil the data response from server or reject the request of data response.
