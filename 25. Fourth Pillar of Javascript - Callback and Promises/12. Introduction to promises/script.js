ff7caadbb980d1068d970f58a914e42b77ea4f9f
Aayush Vyas <aayush25vyas@gmail.com>
2025-05-27 18:29:10 +0530

    Introduction to Promises

---
+++ 25. Fourth Pillar of Javascript - Callback and Promises/12. Introduction to promises/script.js
@@ -0,0 +1,15 @@
+//! Introduction to promises 
+//* We understand that callback function are handle asynchronous task very efficiently but there was problem with those are the callback DOOM or Callback which was the readability problem and second was more bigger problem with callback was Inversion of Control that was harmful for our bussiness logic and also affect our code also. 
+
+//? Instead of Callback function we have Promises which was more enchance version of readability and also solve the concern of Inversion of control. 
+
+//* Promises become powerfull with two features and fill the gap which was not provided by Callback function efficiently. 
+//* 1. Enchance Readability. 
+//* 2. Solve the problem of Inversion of Control 
+
+
+//! -  So we can say that Promises in javascript was special type of object which returned immediately when we call it. 
+
+//! -  Promises act as a placeholder for data we hope to get back sometime in future for example getting data from the backend or third-party url.
+ 
+//! - With this Promise Objects we can attach the some functionality we want to execute once the future task was done and once the future task is complete promises automatically attach the functionality we want to execute after task done. 
