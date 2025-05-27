dbf3764d6e259f44d994deb466823c32c05488b6
Aayush Vyas <aayush25vyas@gmail.com>
2025-05-27 23:49:05 +0530

    How to Create a Promise

--- 25. Fourth Pillar of Javascript - Callback and Promises/13. Important parts of learning promises/script.js
+++ 25. Fourth Pillar of Javascript - Callback and Promises/13. Important parts of learning promises/script.js
@@ -8,3 +8,5 @@
 //? Let understand promises in general term assume that if anyone prmoise you something like they give you some treat when you got A+ grade in your exam if you fulfill the promise that person give you treat and there was anthor condition if you don't get grade A+ grade than you can't get treat so youll don't fullfil the promise 
 
 //* Similarly in techincal term promises work in similar model when we fetch data from backend or third-party api than we use promise this promise will fullfil the data response from server or reject the request of data response.
+//! This promise was mutual agreement between both code function that may be fullfiled or not be fullfiled.
+
---
+++ 25. Fourth Pillar of Javascript - Callback and Promises/14. How to Create a Promise/script.js
@@ -0,0 +1,17 @@
+//! How to create a promises ???? 
+//* In documentation of ECMAscript there was given the reference of promises object it means promises was native feature of javascript handling javascript in synchronous in nature.
+
+//* It means promise object was synchronous in nature that was partially true but the creation of promises was the synchronous. 
+// * As we know that it creates the placeholder for the eventual result of a deferred (it means leave something on later time) that's was asynchronous  
+
+//? Promises have mainly three state : 
+//* 1st. Pending : 
+//?  - When we create the new promise object than pending was the default state.
+//?  - It represent the work in progress state. 
+//* 2nd. Fullfiled : 
+//? If the progress was completed than this pending state was migrated to fullfiled state. 
+//* 3rd. Rejected : 
+//? If the progress was not completed successfully than this pending state was migrated to Rejected state. 
+
+
+
