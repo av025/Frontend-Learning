c186e40ee071cb19dae9bd678f0f3c45bfc19935
Aayush Vyas <aayush25vyas@gmail.com>
2025-05-31 19:01:53 +0530

    Priority Order

---
+++ 26. Fourth Pillar of Javascript - Advanced Promises/12. Priority Order/script.js
@@ -0,0 +1,4 @@
+//! Priority Order 
+//* The Order of Priority in Javascript Runtime was : 
+//? 1st. Global Call-stack "JS native code" -------------> 2nd. Micro-task queue of javascript runtime --------> 3rd. At last we execute Macro-task queue 
+//* This above was order to execute the JS code 
