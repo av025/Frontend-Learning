4d48f0da39f5984d0c134c851ccd6f13b6bff8d8
Aayush Vyas <aayush25vyas@gmail.com>
2025-06-05 22:44:20 +0530

    Introduction to OOPs

---
+++ 28. Sixth Pillar of Javascript - Object Classes  and Prototypes/01. Introduction to OOPs/script.js
@@ -0,0 +1,40 @@
+//! Introduction to OOPs
+//* In Object-Oriented Programming (OOP), we follow the concept of blueprints.
+//* This blueprint is a mental model that represents how a real-world entity looks and behaves.
+
+//* For example, in an e-commerce application, we may have a "Product" entity.
+//* A product has properties like: 
+//   name, model, price, company, review, rating, etc. — these describe the product.
+
+//* A product also performs some actions or behaviors, such as:
+//   buy the product, cancel the order, get product details, add to cart, remove from cart, etc.
+
+//* So, by using this mental blueprint, we can define the structure and behavior of any product.
+
+//? In technical terms, this blueprint is called a **class**.
+
+//* Using this class, we can create multiple real-world products with different data (properties).
+
+// Example:
+// product 1:
+//   name: "Laptop"
+//   model: "15th Generation"
+//   price: 120000
+//   company: "HP"
+//   review: "Best budget and performance laptop"
+//   rating: 4.5
+
+// product 2:
+//   name: "iPhone"
+//   model: "16th Generation"
+//   price: 100000
+//   company: "Apple"
+//   review: "Best phone with great features"
+//   rating: 4.4
+
+//? Technically, these individual products created from the class are called **objects**.
+//* And these objects are just like the JavaScript objects you've already been using.
+
+//*  So in short:
+//   A class is the **blueprint**, and
+//   Objects are the **real instances** created from that blueprint.
