3f5c21c904d677cd4c750c1bab02b70ef24dbb0f
Aayush Vyas <aayush25vyas@gmail.com>
2025-06-05 23:09:48 +0530

    Behaviour and property of Objects

---
+++ 28. Sixth Pillar of Javascript - Object Classes  and Prototypes/02. Behaviour and property of objects/script.js
@@ -0,0 +1,15 @@
+//! Behaviour and Properties of an Object
+
+//* → As we understand, in Object-Oriented Programming (OOP), we use classes to create a blueprint of any real-life entity.
+//* → For example, a `Product` class can define what properties a product has (like name, model, company, price) and what actions (behavior) it can perform (like buy, cancel, add to cart, remove from cart).
+
+//? → One important point: the behavior (i.e., actions/functions) defined in the class remains common for all instances (objects) of that class.
+//? → However, the properties (i.e., values/data) of each object should be unique to distinguish one object from another.
+
+//? → For instance, two products may have the same actions (buy, cancel, etc.), but their data (like name, price, model) should differ at least in one property.
+
+//! → In technical terms:
+//*     - The properties of an object (like `name`, `price`, `company`) are called "Data Members".
+//*     - The actions or methods (like `buyProduct()`, `cancelProduct()`, etc.) are called "Member Functions".
+
+
