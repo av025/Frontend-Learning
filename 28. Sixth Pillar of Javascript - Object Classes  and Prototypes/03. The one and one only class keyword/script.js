d83b456363d96c4ca356378ffb5c102b6d791a6a
Aayush Vyas <aayush25vyas@gmail.com>
2025-06-05 23:26:47 +0530

    The one and one only class keyword

---
+++ 28. Sixth Pillar of Javascript - Object Classes  and Prototypes/03. The one and one only class keyword/script.js
@@ -0,0 +1,16 @@
+//! The one and one only "class" keyword 
+
+//? How can we prepare the blueprint means class in javascript ??? 
+//* With the help of class keyword we can create the classes which create blueprrint for our objects 
+
+//? Syntax of class 
+//  class <class-identifier or class-name> {    
+//?  In this class scope we define the any entity properties 
+//? and behaviour in technical term properties  was called 
+//?  as "Data Member" and behaviour was called as "Member Function"
+//     
+// 
+// } 
+
+
+//* The creation and management of entity was do with two new keyword was "new" and "this" so with the help of this three keyword class, new, and this we create the objects in our javascript. 
