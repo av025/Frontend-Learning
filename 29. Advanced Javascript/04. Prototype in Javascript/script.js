0c05c3edf12a7f9f5e8d6391cd542f6c9bf4db46
Aayush Vyas <aayush25vyas@gmail.com>
2025-06-14 17:26:33 +0530

    Prototype in Javascript Complete

--- 29. Advanced Javascript/04. Prototype in Javascript/script.js
+++ 29. Advanced Javascript/04. Prototype in Javascript/script.js
@@ -18,32 +18,32 @@
 
 // Let create more broader example to understand the prototype 
 
-class Product {
-    productName; 
-    productModel;
-    productPrice;
-    productCompany; 
+// class Product {
+    // productName; 
+    // productModel;
+    // productPrice;
+    // productCompany; 
 
-    constructor(name, model, price, company) {
-        this.productName = name; 
-        this.productModel = model; 
-        this.productPrice = price; 
-        this.productCompany = company; 
-    };
+    // constructor(name, model, price, company) {
+        // this.productName = name; 
+        // this.productModel = model; 
+        // this.productPrice = price; 
+        // this.productCompany = company; 
+    // };
 
-    displayInformation() {
-        console.log(this) //* Refers to Object
-    };
-}; 
+    // displayInformation() {
+        // console.log(this) //* Refers to Object
+    // };
+// }; 
 
-const productOne = new Product("Play Station 5", "Slim Model", 50000, "Sony"); 
-console.log(productOne);
+// const productOne = new Product("Play Station 5", "Slim Model", 50000, "Sony"); 
+// console.log(productOne);
 
-productOne.displayInformation(); 
+// productOne.displayInformation(); 
 //! Now here the question was arise that how this productOne object have this member function displayInformation() if we don't add this into 
 
 //* Now this Product Class have the property of prototype. 
-console.log(Product.prototype); // Output : {} 
+// console.log(Product.prototype); // Output : {} 
 
 //? Let's understand the mechanism of prototype 
 
@@ -78,15 +78,81 @@ console.log(Product.prototype); // Output : {}
 //* Now this buyProduct was associated only to this iphone object not with Product class 
 
 //* Let do prototypal chaining 
-productOne.__proto__.buyProduct = function() {
-    console.log("Product Bought");
-}; 
+// productOne.__proto__.buyProduct = function() {
+    // console.log("Product Bought");
+// }; 
+//! This __proto__ was called the dunder proto with the help of it we can do linkage of our methods and properties to another Object because it set our methods in class constructor associated prototype object also. 
 
-
-console.log(productOne.__proto__); 
+// console.log(productOne.__proto__);  
 //* So with this productOne this buyProduct method was associated if we don't find than we look in this Product.prototype object 
 //? If this propertery was not find in the productOne Object than we see in the Class Object prototype than we don;t see in this Object unamed object so that's how the linking of prototypal inheritance work. 
 
-productOne.displayInformation(); 
+// productOne.displayInformation(); 
 
-//! From tommorrow start from 1:00:00 
+
+//? Let's create the Function Constructor Product 
+// function Product(name, price, company) {
+    // this.productName = name; 
+    // this.productPrice = price; 
+    // this.productComapny = company; 
+// }; 
+
+
+// const productOne = new Product("iphone 16 promax", 120000, "apple"); 
+// console.log(productOne);
+
+//? Now in this Product function constructor we have to manually attach the methods which in class syntax it do by itself 
+// Product.prototype.display = function() {
+    // console.log(this);
+// }; 
+
+// console.log(Product.prototype); 
+//* Output : { display: [Function (anonymous)] }
+
+// productOne.display(); 
+
+
+//? With the help of prototype property we perform very important property of OOPs concept which was Inheritance and it means inherit the properties and methods from the one object to another object and create the prototypal inheritance. 
+
+//* We inherit Classes also but we know that in javascript classes are wrapper and object was create with the help of constructor function. 
+
+//? Parent Class 
+class Events {
+    bookEvents() {
+        console.log("Event was Booked !!!!");
+    }
+}; 
+
+//? Child Class 
+//* Now when we used the extends keyword than it start inherit that class 
+class Movie extends Events {
+   showTime() {
+        console.log("Movie Show Time");
+    };
+}; 
+
+
+//* So here the prototypal inheritance work like this the Movie class prototype object was linked with Events prototype object and the Events prototype object was linked with Object function associated unnamed object. 
+
+class Comdey extends Events {
+    showTime() {
+        console.log("Comdey Show Time");
+    };
+};
+
+
+class Concert extends Events {
+    showTime() {
+        console.log("Concert Show Time");
+    };
+};
+
+
+//* Lets create object 
+
+const movie = new Movie(); 
+ movie.showTime(); 
+//* Can we access the book Events method because we are inherit the Event class 
+movie.bookEvents();  
+//? How we can access this bookEvents so it was access through the protoypal inheritance let see that also : 
+console.log(Movie.prototype.__proto__);
