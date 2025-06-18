c105e57bcaafa2f6db029d47ae6b0d16f55d03e7
Aayush Vyas <aayush25vyas@gmail.com>
2025-06-18 23:46:56 +0530

    Create react app with CRA and Vite

---
+++ Starting with Reactjs/02. Setup the react project with ease/react-app-with-cra/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+     <p>This was App Component !!!!</p>
+    </div>
+  );
+}
+
+export default App;
