afc72b9b8ebeff9924747bc255db6ec2f6ece383
Aayush Vyas <aayush25vyas@gmail.com>
2025-08-15 19:25:12 +0530

    Introduction to Module in NodeJS

---
+++ 37. Understanding the Nodejs/searching.js
@@ -0,0 +1,39 @@
+const linearSearching = function search(arr, x) {
+    for(let i = 0; i < arr.length; i++) {
+
+        if(arr[i] === x) return i; 
+    } 
+
+    return undefined; 
+
+}; 
+
+
+const binarySearching = function search(arr, x) {
+    let lo = 0, hi = arr.length - 1; 
+
+    while(lo <= hi) {
+        let mid = lo + Math.floor((hi - lo)/ 2); 
+        if(arr[mid] === x) return mid; 
+        else if(arr[mid] < x) {
+            lo = mid + 1; 
+        }else {
+            hi = mid - 1; 
+        }
+    }; 
+
+    return undefined;
+}; 
+
+
+// module.exports = {
+//     linearSearch : linearSearching,
+//     binarySearch : binarySearching,
+// }
+
+//* This module.exports was a object only something in codebase we can see this of export also in which we assign key and value pair to it... 
+
+module.exports.linearSearch = linearSearching; 
+module.exports.binarySearch = binarySearching; 
+//? In Key-pair value 
+
